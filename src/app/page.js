68a7b93d6f4a0e9fe4829473a626eb9643506ac7
Babai <biswajit@skynetonline.info>
2024-03-20 16:05:43 +0530

    5th

--- src/app/page.js
+++ src/app/page.js
@@ -39,11 +39,12 @@ export default function Home() {
 
 
 
-  const container = useRef();
-  const circle1 = useRef();
-  const circle2 = useRef();
-  const circle3 = useRef();
+  const container = useRef(0);
+  const circle1 = useRef(0);
+  const circle2 = useRef(0);
+  const circle3 = useRef(0);
   console.log(container.current.offsetLeft);
+
 
   
   // const container1 = document.querySelector('.mainContainer')
@@ -57,94 +58,83 @@ export default function Home() {
 
   const moveFunction = (e) => {
     let target = e.target;
+    alert('working')
+    // const runFunction = () => {
+    //   let mousePos = { x: e.clientX, y: e.clientY };
+    //   if (
+    //     mousePos.x > (container.offsetLeft - (container.clientWidth / 2)) &
+    //     mousePos.x < (container.offsetLeft + ((container.clientWidth / 2) - target.clientHeight)) &
+    //     mousePos.y > (container.offsetTop - (container.clientHeight / 2)) &
+    //     mousePos.y < (container.offsetTop + ((container.clientWidth / 2) - target.clientHeight))
+    //   ) {
+    //     target.style.left = `${mousePos.x}px`
+    //     target.style.top = `${mousePos.y}px`
+    //     setPointer({
+    //       ...pointer,
+    //       x: target.style.left,
+    //       y: target.style.top
+    //     })
+    //   };
 
+    //   if (
+    //     mousePos.x < (container.offsetLeft + (circle3.clientWidth / 2)) &
+    //     mousePos.x > (container.offsetLeft - (circle3.clientWidth / 2)) &
+    //     mousePos.y > (container.offsetTop - (circle3.clientHeight / 2)) &
+    //     mousePos.y < (container.offsetTop + (circle3.clientHeight / 2))
+    //   ) {
+    //     if (
+    //       mousePos.x < (container.offsetLeft + (circle2.clientWidth / 2)) &
+    //       mousePos.x > (container.offsetLeft - (circle2.clientWidth / 2)) &
+    //       mousePos.y > (container.offsetTop - (circle2.clientHeight / 2)) &
+    //       mousePos.y < (container.offsetTop + (circle2.clientHeight / 2))
+    //     ) {
+    //       if (
+    //         mousePos.x < (container.offsetLeft + (circle1.clientWidth / 2)) &
+    //         mousePos.x > (container.offsetLeft - (circle1.clientWidth / 2)) &
+    //         mousePos.y > (container.offsetTop - (circle1.clientHeight / 2)) &
+    //         mousePos.y < (container.offsetTop + (circle1.clientHeight / 2))
+    //       ) {
+    //         setPointer({
+    //           ...pointer,
+    //           x: target.style.left,
+    //           y: target.style.top,
+    //           relation: "Deep"
+    //         })
 
+    //       }
+    //       else {
+    //         setPointer({
+    //           ...pointer,
+    //           x: target.style.left,
+    //           y: target.style.top,
+    //           relation: "Hi/Hello"
+    //         })
 
-    // target.addEventListener('pointerup', () => {
-    //   window.removeEventListener('pointermove', runFunction)
-    // })
-    const runFunction = () => {
-      let mousePos = { x: e.clientX, y: e.clientY };
-      if (
-        // left
-        mousePos.x > (container.offsetLeft - (container.clientWidth / 2)) &
-        // right
-        mousePos.x < (container.offsetLeft + ((container.clientWidth / 2) - target.clientHeight)) &
-        // top
-        mousePos.y > (container.offsetTop - (container.clientHeight / 2)) &
-        // bottom
-        mousePos.y < (container.offsetTop + ((container.clientWidth / 2) - target.clientHeight))
-      ) {
-        target.style.left = `${mousePos.x}px`
-        target.style.top = `${mousePos.y}px`
-        setPointer({
-          ...pointer,
-          x: target.style.left,
-          y: target.style.top
-        })
-      };
+    //       }
 
-      // track circle 1
+    //     }
+    //     else {
+    //       setPointer({
+    //         ...pointer,
+    //         x: target.style.left,
+    //         y: target.style.top,
+    //         relation: "Not Friend"
+    //       })
 
-      if (
-        mousePos.x < (container.offsetLeft + (circle3.clientWidth / 2)) &
-        mousePos.x > (container.offsetLeft - (circle3.clientWidth / 2)) &
-        mousePos.y > (container.offsetTop - (circle3.clientHeight / 2)) &
-        mousePos.y < (container.offsetTop + (circle3.clientHeight / 2))
-      ) {
-        if (
-          mousePos.x < (container.offsetLeft + (circle2.clientWidth / 2)) &
-          mousePos.x > (container.offsetLeft - (circle2.clientWidth / 2)) &
-          mousePos.y > (container.offsetTop - (circle2.clientHeight / 2)) &
-          mousePos.y < (container.offsetTop + (circle2.clientHeight / 2))
-        ) {
-          if (
-            mousePos.x < (container.offsetLeft + (circle1.clientWidth / 2)) &
-            mousePos.x > (container.offsetLeft - (circle1.clientWidth / 2)) &
-            mousePos.y > (container.offsetTop - (circle1.clientHeight / 2)) &
-            mousePos.y < (container.offsetTop + (circle1.clientHeight / 2))
-          ) {
-            setPointer({
-              ...pointer,
-              x: target.style.left,
-              y: target.style.top,
-              relation: "Deep"
-            })
+    //     }
 
-          }
-          else {
-            setPointer({
-              ...pointer,
-              x: target.style.left,
-              y: target.style.top,
-              relation: "Hi/Hello"
-            })
+    //   }
+    //   else {
+    //     setPointer({
+    //       ...pointer,
+    //       x: target.style.left,
+    //       y: target.style.top,
+    //       relation: "Enemy"
+    //     })
+    //   }
+    // }
 
-          }
-
-        }
-        else {
-          setPointer({
-            ...pointer,
-            x: target.style.left,
-            y: target.style.top,
-            relation: "Not Friend"
-          })
-
-        }
-
-      }
-      else {
-        setPointer({
-          ...pointer,
-          x: target.style.left,
-          y: target.style.top,
-          relation: "Enemy"
-        })
-      }
-    }
-
-    window.addEventListener('pointermove', runFunction)
+    // window.addEventListener('pointermove', runFunction)
   }
 
 
