d5c195dcce0164ece2e1a8fba3c3d463d0cc4fe0
Babai <biswajit@skynetonline.info>
2024-03-21 16:47:22 +0530

    21-3

--- src/app/page.js
+++ src/app/page.js
@@ -5,40 +5,14 @@ import { useState, useEffect, useRef } from "react"
 
 
 export default function Home() {
-
+  const [hasMounted, setHasMounted] = useState(false);
   const [pointer, setPointer] = useState([])
-  const [name, setName] = useState(null)
+  const [save, setSave] = useState(1)
   const [data, setData] = useState(null)
 
-
-
-  // form submission
-  const handleSubmit = (event) => {
-    event.preventDefault()
-
-    setPointer(
-      [
-        ...pointer,
-
-        {
-          persone: name,
-          x: '',
-          y: '',
-          relation: ''
-        }
-      ]
-    )
-  }
-
-  // save Local strage
-  useEffect(() => {
-    localStorage.setItem('users', JSON.stringify(pointer));
-    setData(JSON.parse(localStorage.getItem('users')))
-
-  }, [pointer]);
-
-
-
+  const [name, setName] = useState(null)
+  const [phone, setPhone] = useState(null)
+  const [email, setEmail] = useState('')
 
 
   const container = useRef(0);
@@ -46,6 +20,39 @@ export default function Home() {
   const circle2 = useRef(0);
   const circle3 = useRef(0);
 
+
+
+
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+
+  // form submission
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    let random = Math.random()
+    let idNo = random.toString().split('.')[1]
+
+    setPointer(
+      [
+        ...pointer,
+
+        {
+          id: idNo,
+          persone: name,
+          phone: phone,
+          email: email,
+          x: '',
+          y: '',
+          relation: ''
+        }
+      ]
+    )
+    setSave(save + 1)
+  }
 
 
 
@@ -63,23 +70,18 @@ export default function Home() {
         target.style.left = `${mousePos.x}px`
         target.style.top = `${mousePos.y}px`
 
+        let allData = pointer
 
-        let oldData = pointer.filter((item) => (
-          item.persone !== target.getAttribute('data-id')
-        ))
+        allData.forEach(item => {
+          if (item.id === target.getAttribute('data-id')) {
+            item.x = target.style.left,
+              item.y = target.style.top
+          }
+        });
+        setPointer(allData)
+        setSave(mousePos.x + mousePos.y)
 
-
-        setPointer(
-          [
-            ...oldData,
-            {
-              persone: name,
-              x: target.style.left,
-              y: target.style.top,
-              relation: ''
-            }
-          ]
-        )
+        console.log(pointer);
       };
 
       if (
@@ -100,81 +102,59 @@ export default function Home() {
             mousePos.y > (container.current.offsetTop - (circle1.current.clientHeight / 2)) &
             mousePos.y < (container.current.offsetTop + (circle1.current.clientHeight / 2))
           ) {
-            let oldData = pointer.filter((item) => (
-              item.persone !== target.getAttribute('data-id')
-            ))
-            setPointer(
-              [
-                ...oldData,
-                {
-                  persone: name,
-                  x: target.style.left,
-                  y: target.style.top,
-                  relation: "Deep"
-                }
-              ]
-            )
 
-
+            let allData = pointer
+            allData.forEach(item => {
+              if (item.id === target.getAttribute('data-id')) {
+                item.x = target.style.left,
+                  item.y = target.style.top,
+                  item.relation = "Friend"
+              }
+            });
+            setPointer(allData)
+            setSave(mousePos.x + mousePos.y)
           }
           else {
-            let oldData = pointer.filter((item) => (
-              item.persone !== target.getAttribute('data-id')
-            ))
-            setPointer(
-              [
-                ...oldData,
-                {
-                  persone: name,
-                  x: target.style.left,
-                  y: target.style.top,
-                  relation: "Hi/Hello"
-                }
-              ]
-            )
+            let allData = pointer
+            allData.forEach(item => {
+              if (item.id === target.getAttribute('data-id')) {
+                item.x = target.style.left,
+                  item.y = target.style.top,
+                  item.relation = "Hi/Hello"
+              }
+            });
+            setPointer(allData)
+            setSave(mousePos.x)
 
 
           }
 
         }
         else {
-          let oldData = pointer.filter((item) => (
-            item.persone !== target.getAttribute('data-id')
-          ))
-          setPointer(
-            [
-              ...oldData,
-              {
-                persone: name,
-                x: target.style.left,
-                y: target.style.top,
-                relation: "Not Friend"
-              }
-            ]
-          )
-
-
-
+          let allData = pointer
+          allData.forEach(item => {
+            if (item.id === target.getAttribute('data-id')) {
+              item.x = target.style.left,
+                item.y = target.style.top,
+                item.relation = "Not Friend"
+            }
+          });
+          setPointer(allData)
+          setSave(mousePos.x + mousePos.y)
         }
 
       }
       else {
-        let oldData = pointer.filter((item) => (
-          item.persone !== target.getAttribute('data-id')
-        ))
-        setPointer(
-          [
-            ...oldData,
-            {
-              persone: name,
-              x: target.style.left,
-              y: target.style.top,
-              relation: "Enemy"
-            }
-          ]
-        )
-
-
+        let allData = pointer
+        allData.forEach(item => {
+          if (item.id === target.getAttribute('data-id')) {
+            item.x = target.style.left,
+              item.y = target.style.top,
+              item.relation = "Enemy"
+          }
+        });
+        setPointer(allData)
+        setSave(mousePos.x + mousePos.y)
       }
 
       window.addEventListener('pointerup', () => {
@@ -191,9 +171,53 @@ export default function Home() {
 
 
 
+  // save Local strage
+  useEffect(() => {
+    localStorage.setItem('users', JSON.stringify(pointer));
+    setData(JSON.parse(localStorage.getItem('users')))
+
+  }, [save]);
 
 
-  return (
+
+
+  //Tab
+
+  const tabFunction = () => {
+    if (typeof window !== "undefined") {
+      const tabLink = document.querySelectorAll('.tabLinks li')
+      const tabContent = document.querySelectorAll('.tabContainer')
+      tabLink.forEach(item => {
+        item.addEventListener('click', ()=>{
+          tabContent.forEach(content => {
+            if(content.getAttribute('id') === item.getAttribute('data-id')){
+              content.style.display="block"
+            }
+            else{
+              content.style.display="none"
+            }
+          });
+        })
+      });
+    }
+
+  }
+
+
+
+  const handleTab = (e) => {
+    e.target.style.color = '#000'
+    if (e.target.getAttribute('data-id') === 'tab1') {
+      tab1.current.style.display = 'block'
+    }
+    if (e.target.getAttribute('data-id') === 'tab2') {
+      tab2.current.style.display = 'block'
+    }
+  }
+
+
+
+  return hasMounted && (
     <>
 
 
@@ -207,7 +231,7 @@ export default function Home() {
 
       {data !== null ?
         data.map((item, index) => (
-          <div key={index} data-id={item.persone} className="dragBox" onPointerDown={moveFunction} ></div>
+          <div key={index} data-id={item.id} className="dragBox" title={item.persone} onPointerDown={moveFunction} ></div>
         ))
         : null}
 
@@ -217,26 +241,59 @@ export default function Home() {
 
         </div>
         <form onSubmit={handleSubmit} className="mb-2">
-          <div className="mb-1"><input type="text" id="name" name="name" onChange={(e) => { setName(e.target.value) }} className="form-control" placeholder="Name" /></div>
-          <button type="submit" className="btn btn-sm btn-success">Add</button>
+          <div className="mb-1"><input type="text" id="name" name="name" onChange={(e) => { setName(e.target.value) }} className="form-control" placeholder="Name" required /></div>
+          <div className="mb-1"><input type="text" id="phone" name="phone" onChange={(e) => { setPhone(e.target.value) }} className="form-control" placeholder="Phone" required /></div>
+          <div className="mb-1"><input type="email" id="email" name="email" onChange={(e) => { setEmail(e.target.value) }} className="form-control" placeholder="Email" /></div>
+          <button type="submit" className="btn btn-sm btn-success">Create</button>
         </form>
         <div className="dataList">
           <h4>Relation List</h4>
-          {data !== null ?
-            data.map((item, index) => (
-              <div key={index} className="relationBlock">
-                Name: {item.persone}<br />
-                Relation: {item.relation}<br />
-                X:({item.x}) ,
-                Y:({item.y})
-              </div>
-            ))
-            : null}
+          <div className="tabBox" >
+            <ul className="tabLinks d-flex gap-3 mb-1" >
+              <li data-id="tab1">All</li>
+              <li data-id="tab2">Friends</li>
+            </ul>
+            <div className="tabContainer" id="tab1" >
+              <p>All Mwmber</p>
+              {data !== null ?
+                data.map((item, index) => (
+                  <div key={index} className="relationBlock">
+
+                    {item.persone}<br />
+                    {item.phone}<br />
+                    {item.email}<br />
+                    Relation: <strong>{item.relation}</strong> <br />
+                    X:({item.x}) ,
+                    Y:({item.y})<br />
+                    <small>ID: {item.id}</small>
+                  </div>
+                ))
+                : '0 relative'}
+            </div>
+            <div className="tabContainer" id="tab2">
+            <p>All Friend</p>
+              {data !== null ?
+                data.map((item, index) => (
+                  <div key={index} className="relationBlock">
+
+                    {item.persone}<br />
+                    {item.phone}<br />
+                    {item.email}<br />
+                    Relation: <strong>{item.relation}</strong> <br />
+                    X:({item.x}) ,
+                    Y:({item.y})<br />
+                    <small>ID: {item.id}</small>
+                  </div>
+                ))
+                : '0 friend'}
+            </div>
+          </div>
+
         </div>
 
       </div >
 
-
+      {console.log(tabFunction())}
 
     </>
   );
